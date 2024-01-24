609fdcec561c731e32de5ebf708e3f21f9372710
Felix Ngitari <106065411+Felix273@users.noreply.github.com>
2024-01-24 04:38:11 -0800

    Add files via upload

---
+++ StatementExpression/src/Main.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val myNumber = 150
+    if(myNumber >= 150){
+        println("Greater than 150")
+    }else if(myNumber > 90){
+        println("Greater than 90")
+    }else{
+        println("All the conditions are false")
+    }
+}
